2abfa873235fba98e762db7c24e4426e2c296218
GuiDDuran <guilherme.d.gea@gmail.com>
2025-03-27 21:31:49 -0300

    configurando porta

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.database import db
+import os
 
 app = create_app()
 
@@ -8,4 +9,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    # Obter a porta do ambiente (Azure App Service) ou usar 8000 como padrão
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
